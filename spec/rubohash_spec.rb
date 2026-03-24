33769badeacd6504ddc17b667bc569a8c0345ff3
Nedzib Sastoque <nedzib.sastoque@gmail.com>
2026-03-24 18:08:42 -0500

    Bump gem to v0.1.4

--- spec/rubohash_spec.rb
+++ spec/rubohash_spec.rb
@@ -1,6 +1,6 @@
 RSpec.describe Rubohash do
   it 'has a version number' do
-    expect(Rubohash::VERSION).to eql('0.1.3')
+    expect(Rubohash::VERSION).to eql('0.1.4')
   end
 
   describe 'concerning defaults' do
